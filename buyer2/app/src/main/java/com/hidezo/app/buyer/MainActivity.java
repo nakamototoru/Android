f972c638fb5e102409eacc55e65f1bc5c0fd0d97
デザミシステム <akira@dezamisystem.com>
2017-03-12 19:35:12 +0900

    申請ビルド

--- buyer2/app/src/main/java/com/hidezo/app/buyer/MainActivity.java
+++ buyer2/app/src/main/java/com/hidezo/app/buyer/MainActivity.java
@@ -2,8 +2,10 @@ package com.hidezo.app.buyer;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-//import android.content.SharedPreferences;
-//import android.preference.PreferenceManager;
+import android.content.pm.PackageManager;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,12 +15,14 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
+import android.Manifest.permission;
 
 /**
  * ログインフォーム画面・最初に呼び出される
  */
 public class MainActivity extends AppCompatActivity implements HDZClient.HDZCallbacks {
+
+    String MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE = "MY_PERMISSIONS_REQUEST_ACCESS_NETWORK_STATE";
 
     String myLoginId = "";
 
